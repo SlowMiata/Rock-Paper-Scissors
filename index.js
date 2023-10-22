325e6008815babab77ceccd167023ec79b451e02
Tu <tun42092@temple.edu>
2023-10-22 19:16:08 -0400

    completed basic game

--- index.js
+++ index.js
@@ -28,58 +28,67 @@ function PlayRound(PlayerChoice){
         case "rock":
             if(robot == "rock"){
                 console.log("tie")
-                return "tie"
+                return 0
             }
             else if (robot == "paper"){
                 console.log("you lose :C")
-                return ("you losE :c")
+                return 0
             }
             else if (robot == "scissors"){
                 console.log("you win C:")
-                return "you win C:"
+                return 1
             }
             break
         
         case "paper":
             if(robot == "paper"){
                 console.log("tie")
-                return "tie"
+                return 0
             }
             else if (robot == "scissors"){
                 console.log("you lose :C")
-                return ("you losE :c")
+                return 0
             }
             else if (robot == "rock"){
                 console.log("you win C:")
-                return "you win C:"
+                return 1
             }
             break
 
         case "scissors":
             if(robot == "scissors"){
                 console.log("tie")
-                return "tie"
+                return 0
             }
             else if (robot == "rock"){
                 console.log("you lose :C")
-                return ("you losE :c")
+                return 0
             }
             else if (robot == "paper"){
                 console.log("you win C:")
-                return "you win C:"
+                return 1
             }
             break
 
         default:
             console.log("invalid choice")
-            return "invalid"
+            console.log(PlayerChoice)
+            return 0
         
     }
     
 }
 
-function main(){
-    let choice = prompt("rock, paper scissors shoot");
-    PlayRound(choice.toLowerCase)
+function game(){
+
+    let score = 0
+
+
+    for(let i = 0; i < 5; i++){
+        let choice = prompt("rock, paper scissors shoot");
+        score  += PlayRound(choice.toLowerCase())
+    }
+    console.log("total score is " + score)
 }
-main()
+
+game()
