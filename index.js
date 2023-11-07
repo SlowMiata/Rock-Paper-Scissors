09f37e96ec0faee81d097ae6ba350e051c889999
Tu <tun42092@temple.edu>
2023-11-07 12:26:21 -0500

    display scores

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const result = document.querySelector('.result')
 const robotResult = document.querySelector(".robot")
 const playerResult = document.querySelector(".player")
 const scoreDisplay = document.querySelector('.score')
+const winner = document.querySelector('.winner')
+const currentScore = document.querySelector('.currentScore')
 
 function getComputerChoice(){
     //gets the choice of the robot at random
@@ -22,10 +24,10 @@ function getComputerChoice(){
 
 }
 
-
-
 //play a single round
 function PlayRound(PlayerChoice){
+    scoreDisplay.textContent = ''
+    winner.textContent = ''
 
     playerResult.textContent = PlayerChoice
 
@@ -83,35 +85,49 @@ function PlayRound(PlayerChoice){
             return 0
         
     }
-    
 }
-
+    
 
 function game(){
 
     let score = 0
     let gameCount = 0
 
-    const rock = document.querySelector('.rock').addEventListener('click', () =>{  
+    document.querySelector('.rock').addEventListener('click', () =>{  
         gameCount += 1 
-        score += PlayRound('rock') 
+        score += PlayRound('rock')
+        currentScore.textContent = score
         checkGameCountAndDisplayScore()
     })
     document.querySelector('.paper').addEventListener('click', () =>{   
         gameCount += 1
-        score += PlayRound('paper') 
+        score += PlayRound('paper')
+        currentScore.textContent = score
         checkGameCountAndDisplayScore()
     })
     document.querySelector('.scissors').addEventListener('click', () =>{   
         gameCount += 1
         score += PlayRound('scissors') 
+        currentScore.textContent = score
         checkGameCountAndDisplayScore()
     })
+
+    
 
     function checkGameCountAndDisplayScore() {
         if (gameCount === 5) {
             scoreDisplay.textContent = `Game over! Your score is ${score}`;
+                if (score <3 ){
+
+                    winner.textContent = "Robot Wins!"
+                }
+                else{
+                    winner.textContent = "Player Wins!"
+                }
             gameCount = 0
+            score = 0
+            currentScore.textContent = score
+            
         }
     }
 
@@ -119,6 +135,5 @@ function game(){
 
 
 game()
-
 
 
